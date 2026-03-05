9c0ccd2160f9a281ecded16fb60b751238e18f2e
Alex Pandian M <alexpandian.m@mitrahsoft.in>
2026-03-06 00:25:05 +0530

    Update main.js

--- Todo_App/assets/main.js
+++ Todo_App/assets/main.js
@@ -1,173 +1,177 @@
-$(document).ready(function () {
+$(document).ready(function(){
+
+function renderTodos(data){
+
+    var html = "";
+
+    for(var i=0;i<data.DATA.length;i++){
+
+        var id = data.DATA[i][0];
+        var text = data.DATA[i][1];
+        var done = data.DATA[i][2];
+
+        var checked = done == 1 ? "checked" : "";
+        var line = done == 1 ? "text-decoration-line-through text-muted" : "";
+
+        html += `
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <div>
+                <input type="checkbox" class="item me-2" data-id="${id}" ${checked}>
+                <span class="${line}">
+                    ${text}
+                </span>
+            </div>
+            <button class="deleteBtn btn btn-sm" data-id="${id}">✕</button>
+        </li>
+        `;
+    }
+
+    $("#todoList").html(html);
+
+}
+
+
+function loadTodos(type){
+
+    $.ajax({
+
+        url:"index.cfm?action=main.filter",
+        type:"get",
+        dataType:"json",
+        data:{type:type},
+
+        success:function(res){
+
+            renderTodos(res);
+
+        }
+
+    });
+
+}
+
+
+
+$("#paraText").keypress(function(e){
+
+    if(e.which == 13){
+
+        var txt = $("#paraText").val().trim();
+
+        if(txt == "") return;
+
+        $.ajax({
+
+            url:"index.cfm?action=main.save",
+            type:"post",
+            dataType:"json",
+            data:{todo_text:txt},
+
+            success:function(){
+
+                $("#paraText").val("");
+
+                loadTodos("all");
+
+            }
+
+        });
+
+    }
+
+});
+
+
+
+$("#allBtn").click(function(){
 
     loadTodos("all");
 
+});
 
-    $("#paraText").on("keydown", function (e) {
 
-        if (e.key === "Enter") {
+$("#activeBtn").click(function(){
 
-            e.preventDefault();
+    loadTodos("active");
 
-            var txt = $("#paraText").val().trim();
-            if (txt === "") return;
+});
 
-            $.ajax({
-                url: "index.cfm?action=main.save",
-                type: "POST",
-                dataType: "json",
-                data: { todo_text: txt },
 
-                success: function (res) {
+$("#completedBtn").click(function(){
 
-                    $("#paraText").val("");
-                    loadTodos("all");
+    loadTodos("completed");
 
-                }
-            });
+});
+
+
+
+$(document).on("click",".deleteBtn",function(){
+
+    var id = $(this).data("id");
+
+    $.ajax({
+
+        url:"index.cfm?action=main.delete",
+        type:"post",
+        dataType:"json",
+        data:{id:id},
+
+        success:function(){
+
+            loadTodos("all");
 
         }
 
     });
 
-
-
-    $(document).on("click", ".deleteBtn", function () {
-
-        var id = $(this).data("id");
-
-        $.ajax({
-            url: "index.cfm?action=main.delete",
-            type: "POST",
-            dataType: "json",
-            data: { id: id },
-
-            success: function () {
-
-                loadTodos("all");
-
-            }
-        });
-
-    });
+});
 
 
 
-    $(document).on("change", ".item", function () {
+$(document).on("change",".item",function(){
 
-        var id = $(this).data("id");
-        var done = $(this).is(":checked") ? 1 : 0;
+    var id = $(this).data("id");
 
-        $.ajax({
-            url: "index.cfm?action=main.toggle",
-            type: "POST",
-            dataType: "json",
-            data: { id: id, isDone: done },
+    var done = $(this).is(":checked") ? 1 : 0;
 
-            success: function () {
+    $.ajax({
 
-                loadTodos("all");
+        url:"index.cfm?action=main.toggle",
+        type:"post",
+        dataType:"json",
+        data:{
+            id:id,
+            isDone:done
+        },
 
-            }
-        });
+        success:function(){
 
-    });
-
-
-
-    $("#allBtn").click(function () {
-
-        loadTodos("all");
-
-    });
-
-    $("#activeBtn").click(function () {
-
-        loadTodos("active");
-
-    });
-
-    $("#completedBtn").click(function () {
-
-        loadTodos("completed");
-
-    });
-
-
-
-    $("#clearCompletedBtn").click(function () {
-
-        $.ajax({
-            url: "index.cfm?action=main.clearCompleted",
-            type: "POST",
-            dataType: "json",
-
-            success: function () {
-
-                loadTodos("all");
-
-            }
-        });
-
-    });
-
-
-
-
-    function loadTodos(type) {
-
-        $.ajax({
-            url: "index.cfm?action=main.filter",
-            type: "GET",
-            dataType: "json",
-            data: { type: type },
-
-            success: function (data) {
-
-                renderTodos(data);
-
-            }
-        });
-
-    }
-
-
-
-
-    function renderTodos(data) {
-
-        var html = "";
-        var left = 0;
-
-        if (data.DATA) {
-
-            $.each(data.DATA, function (i, row) {
-
-                var id = row[0];
-                var text = row[1];
-                var done = row[2];
-
-                if (done == 0) left++;
-
-                html += `
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                    <div>
-                        <input type="checkbox" class="item me-2" data-id="${id}" ${done == 1 ? "checked" : ""}>
-                        <span class="${done == 1 ? "text-decoration-line-through text-muted" : ""}">
-                            ${text}
-                        </span>
-                    </div>
-                    <button class="deleteBtn btn btn-sm" data-id="${id}">✕</button>
-                </li>`;
-
-            });
+            loadTodos("all");
 
         }
 
-        $("#todoList").html(html);
+    });
 
-        $("#itemsLeft").text(left + " items left");
+});
 
-    }
+
+
+$("#clearCompletedBtn").click(function(){
+
+    $.ajax({
+
+        url:"index.cfm?action=main.clear",
+        type:"post",
+        dataType:"json",
+
+        success:function(){
+
+            loadTodos("all");
+
+        }
+
+    });
+
+});
 
 });
